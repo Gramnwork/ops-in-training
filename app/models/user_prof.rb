3299cdc63f6fbf5299572c07133d77e3f7a4f068
gramn <gramn@gramn-no-Mac-mini.local>
2016-01-27 15:10:52 +0900

    女性のみ抽出して表示

--- app/models/user_prof.rb
+++ app/models/user_prof.rb
@@ -1,7 +1,10 @@
 class UserProf < ActiveRecord::Base
 
-	#3-2. バリデーション追加｜name10文字以上、性別必須
+	# 3-2. バリデーション追加｜name10文字以上、性別必須
 	validates :name, length: { minimum: 10 } 
 	validates :age, presence: true
+
+	# 3-3. User一覧に女性のみ表示
+	scope :user_female, -> {where(male: false)}
 
 end
